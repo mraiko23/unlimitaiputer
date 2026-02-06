8d4deedac76ecd9b405afb63e1194a74e78ff2e5
mraiko23 <aiearnideas@gmail.com>
2026-02-06 17:38:26 +0700

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -7,6 +7,6 @@ npm install
 
 echo "Installing Chrome for Puppeteer..."
 # Install Chrome to a local cache directory
-npx puppeteer browsers install chrome --path ./.cache/puppeteer
+npx puppeteer browsers install chrome --path "$(pwd)/.cache/puppeteer"
 
 echo "Build complete."
